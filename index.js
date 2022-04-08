d1d21034b0869ce56491cb26b96a17193a046b64
Edivaldo Pereira <edivaldo.pereira@gmail.com>
2022-04-08 09:19:55 -0300

    Dados dos clientes

---
+++ index.js
@@ -0,0 +1,10 @@
+const cliente1Nome = "Ricardo"
+const cliente1CPF = 11111325456
+const cliente1Agencia = 0001
+const cliente1Saldo = 0
+
+const cliente2Nome = "Alice"
+const cliente2CPF = 21111325432
+const cliente2Agencia = 0001
+const cliente2Saldo = 0
+
