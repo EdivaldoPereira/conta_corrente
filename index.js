de43c4e8df09af296eb8e0c01346dae889815e8a
Edivaldo Pereira <edivaldo.pereira@gmail.com>
2022-04-09 20:53:56 -0300

    Correção da linha 2, onde foi incluído o from

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import {Cliente} from "./Cliente.js"
-import {ContaCorrente} "./ContaCorrente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
 
 const cliente1 = new Cliente()
 cliente1.nome = "Ricardo"
