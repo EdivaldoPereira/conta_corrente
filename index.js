e5325790b226a450655dddc4164ef3c34fc24c81
Edivaldo Pereira <edivaldo.pereira@gmail.com>
2022-04-10 18:25:13 -0300

    Salvando index.js

--- index.js
+++ index.js
@@ -12,10 +12,13 @@ cliente2.cpf = 21111325432
 const conta1 = new ContaCorrente()
 conta1.agencia = 1001
 conta1.cliente = cliente1
+conta1.depositar(500)
 
 const conta2 = new ContaCorrente()
 conta2.cliente = cliente2
 conta2.agencia = 102
+
+conta1.transferir(200, conta2)
 
 console.log(conta1)
 console.log(conta2)
