4f524b3db4197f88bc1f4a1bfebc0dd13da707f7
Edivaldo Pereira <edivaldo.pereira@gmail.com>
2022-04-11 08:29:07 -0300

    Criação da classe Conta

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
 import { ContaPoupanca } from "./ContaPoupanca.js"
+import { Conta } from "./Conta.js"
 
 const cliente1 = new Cliente("Ricardo", 11111325456)
 
-const conta1 = new ContaCorrente(1001, cliente1)
-conta1.depositar(500)
-conta1.sacar(100)
+const contaCorrente = new Conta(0, cliente1, 1001)
+contaCorrente.depositar(500)
+contaCorrente.sacar(100)
 
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001)
+const contaPoupanca = new Conta(50, cliente1, 1001)
 
-console.log(conta1)
+console.log(contaCorrente)
 console.log(contaPoupanca)
 console.log("O número total de contas é ", ContaCorrente.numeroDeContas)
 
