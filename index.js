efa21833251d9d06486388955c5ea0a631fc2fcb
Edivaldo Pereira <edivaldo.pereira@gmail.com>
2022-04-11 08:16:29 -0300

    Correção da classe Conta Poupança

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
+import { ContaPoupanca } from "./ContaPoupanca.js"
 
 const cliente1 = new Cliente("Ricardo", 11111325456)
 
-const cliente2 = new Cliente("Alice", 21111325432)
-
 const conta1 = new ContaCorrente(1001, cliente1)
 conta1.depositar(500)
+conta1.sacar(100)
 
-const conta2 = new ContaCorrente(102, cliente2)
-
-conta1.transferir(200, conta2)
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001)
 
 console.log(conta1)
-console.log(conta2)
+console.log(contaPoupanca)
 console.log("O número total de contas é ", ContaCorrente.numeroDeContas)
 
