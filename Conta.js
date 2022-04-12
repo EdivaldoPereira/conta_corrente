f1a5562c33e7a1081d6bf060bd2c5cd7773bf378
Edivaldo Pereira <edivaldo.pereira@gmail.com>
2022-04-12 09:49:48 -0300

    Criação dos tipos salario e empresarial

--- Conta.js
+++ Conta.js
@@ -18,13 +18,19 @@ export class Conta{
     }
     sacar(valor){
         let = taxa = 1
-        if(this._tipo == "corrente"{
+        if(this._tipo == "corrente"){
             taxa = 1.1
-        })
+        }
+        if(this._tipo == "salario"){
+            taxa = 1.05
+        }
+        if(this._tipo == "empresarial"){
+            taxa = 1.15
+        }
         const valorSacado = taxa * valor
-        if(this._saldo >= valor){
-            this._saldo -= valor
-            return valor
+        if(this._saldo >= valorSacado){
+            this._saldo -= valorSacado
+            return valorSacado
         }
     }
 
@@ -36,6 +42,9 @@ export class Conta{
     }
 
     transferir(valor, conta){
+        if(this._tipo == "salario"){
+            return
+        }
         const valorSacado = this.sacar(valor)
         conta.depositar(valorSacado)
     }
