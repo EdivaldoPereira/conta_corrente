f663cd1a272bda81a6684bb26a401b2eafb618f2
Edivaldo Pereira <edivaldo.pereira@gmail.com>
2022-04-13 07:58:39 -0300

    Inclusão da classe Error

--- Conta.js
+++ Conta.js
@@ -1,11 +1,12 @@
 export class Conta{
     constructor(saldoInicial, cliente, agencia){
+        if(this.constructor == Conta){
+            throw new Error("Você não deveria instanciar um objeto do tipo conta")
+        }
         this._saldo = saldoInicial
         this._cliente = cliente
         this._agencia = agencia
-        if(this.constructor == Conta){
-            console.log("Você não deveria instanciar um objeto do tipo conta")
-        }
+        
     }
     set cliente(novoValor){
         if(novoValor instanceof Cliente)
