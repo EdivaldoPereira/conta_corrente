7b0527845b3327170f46666cf5b6361c85e963c6
Edivaldo Pereira <edivaldo.pereira@gmail.com>
2022-04-14 12:37:57 -0300

    Finalização do código

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ const gerente = new Gerente("Ricardo", 5000, 12345678901)
 gerente.cadastrarSenha("012345")
 
 const cliente = new Cliente("Lais", 45678916541, "123")
+
 const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456")
 const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "012345")
 const clienteEstaLogado = SistemaAutenticacao.login(cliente, "123")
 
-console.log(diretorEstaLogado, "\n", gerenteEstaLogado)
+console.log(diretorEstaLogado,"\n",gerenteEstaLogado)
 console.log(clienteEstaLogado)
