33a9966ac6f465a0dda162e4a185daa7eea51070
Edivaldo Pereira <edivaldo.pereira@gmail.com>
2022-04-14 10:11:26 -0300

    Configuração de método de acesso

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
 import {Cliente} from "./Cliente.js"
-import {ContaCorrente} from "./Conta/ContaCorrente.js"
-import { ContaPoupanca } from "./Conta/ContaPoupanca.js"
-import { ContaSalario } from "./Conta/ContaSalario.js"
+import { Gerente } from "./Funcionario/Gerente.js"
+import { Diretor } from "./Funcionario/Diretor.js"
 
+const diretor = new Diretor("Rodrigo", 10000, 12345678900)
+const gerente = new Gerente("Ricardo", 5000, 12345678901)
 
-const cliente1 = new Cliente("Ricardo", 11111325456)
-
-const contaCorrente = new ContaCorrente(0, cliente1, 1001)
-contaCorrente.depositar(500)
-contaCorrente.sacar(100)
-
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001)
-const contaSalario = new ContaSalario(cliente1)
-contaSalario.depositar(100)
-contaSalario.sacar(10)
-
-console.log(contaCorrente)
-console.log(contaPoupanca)
-
-console.log(contaSalario)
+SistemaAutenticacao.login(diretor, "123456")
